1ef50615f814cd8673ce7da7f15d13f3813730ee
carlrobertkarell-beep <carlrobert.karell@gmail.com>
2026-08-13 08:31:50 +0200

    Add files via upload
    V3

--- config.js
+++ config.js
@@ -2,4 +2,5 @@
 window.KANSEI = {
   kampanjSlut: "30 september",   // visas i alla "t.o.m."-texter
   bokning: "https://www.bokadirekt.se/places/kansei-rehabcenter-48847"            // målet för generella Boka tid-knappar (tjänstespecifika länkar påverkas inte)
+  ,halsningsvideo: ""             // sätt till "/assets/halsning.mp4" när videon är uppladdad
 };
